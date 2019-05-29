55c40533df6b5240556d9b99446dddfbf0754406
Florian Klein <35765059+TheFK@users.noreply.github.com>
2019-05-30 00:47:40 +0200

    v1

---
+++ Galaxy Towers Launcher/Form_Launcher.Designer.vb
@@ -0,0 +1,123 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form_Launcher
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.ListBox_status = New System.Windows.Forms.ListBox()
+        Me.Button_play = New System.Windows.Forms.Button()
+        Me.Button_forceUpdate = New System.Windows.Forms.Button()
+        Me.Button_close = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Galaxy_Towers_Launcher.My.Resources.Resources._4e191dea_10e6_47be_8c79_ea722aeb2f12
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(438, 215)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'ListBox_status
+        '
+        Me.ListBox_status.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.ListBox_status.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.ListBox_status.ForeColor = System.Drawing.Color.White
+        Me.ListBox_status.FormattingEnabled = True
+        Me.ListBox_status.Location = New System.Drawing.Point(12, 233)
+        Me.ListBox_status.Name = "ListBox_status"
+        Me.ListBox_status.Size = New System.Drawing.Size(438, 236)
+        Me.ListBox_status.TabIndex = 1
+        '
+        'Button_play
+        '
+        Me.Button_play.BackColor = System.Drawing.Color.White
+        Me.Button_play.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Button_play.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button_play.Font = New System.Drawing.Font("Calibri", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button_play.ForeColor = System.Drawing.Color.Black
+        Me.Button_play.Location = New System.Drawing.Point(254, 518)
+        Me.Button_play.Name = "Button_play"
+        Me.Button_play.Size = New System.Drawing.Size(196, 64)
+        Me.Button_play.TabIndex = 2
+        Me.Button_play.Text = "Play!"
+        Me.Button_play.UseVisualStyleBackColor = False
+        '
+        'Button_forceUpdate
+        '
+        Me.Button_forceUpdate.BackColor = System.Drawing.Color.White
+        Me.Button_forceUpdate.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Button_forceUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button_forceUpdate.Font = New System.Drawing.Font("Calibri", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button_forceUpdate.ForeColor = System.Drawing.Color.Black
+        Me.Button_forceUpdate.Location = New System.Drawing.Point(12, 518)
+        Me.Button_forceUpdate.Name = "Button_forceUpdate"
+        Me.Button_forceUpdate.Size = New System.Drawing.Size(196, 64)
+        Me.Button_forceUpdate.TabIndex = 3
+        Me.Button_forceUpdate.Text = "Force Update!"
+        Me.Button_forceUpdate.UseVisualStyleBackColor = False
+        '
+        'Button_close
+        '
+        Me.Button_close.BackColor = System.Drawing.Color.Red
+        Me.Button_close.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Button_close.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Button_close.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button_close.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button_close.ForeColor = System.Drawing.Color.White
+        Me.Button_close.Location = New System.Drawing.Point(423, 12)
+        Me.Button_close.Name = "Button_close"
+        Me.Button_close.Size = New System.Drawing.Size(27, 26)
+        Me.Button_close.TabIndex = 4
+        Me.Button_close.Text = "X"
+        Me.Button_close.UseVisualStyleBackColor = False
+        '
+        'Form_Launcher
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(462, 594)
+        Me.Controls.Add(Me.Button_close)
+        Me.Controls.Add(Me.Button_forceUpdate)
+        Me.Controls.Add(Me.Button_play)
+        Me.Controls.Add(Me.ListBox_status)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.ForeColor = System.Drawing.Color.White
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form_Launcher"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Galaxy Towers Launcher"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents ListBox_status As ListBox
+    Friend WithEvents Button_play As Button
+    Friend WithEvents Button_forceUpdate As Button
+    Friend WithEvents Button_close As Button
+End Class
---
+++ Galaxy Towers Launcher/Form_Launcher.vb
@@ -0,0 +1,75 @@
+﻿Imports System.IO
+Imports System.IO.Compression
+Imports System.Threading
+
+Public Class Form_Launcher
+
+    Public version_address As String = "http://galaxytowers.altervista.org/update/version.txt"
+    Public local_ver_address As String = "game_ver.txt"
+    Public download_address As String = "http://galaxytowers.altervista.org/update/Galaxy_Towers.zip"
+    Public client As New Net.WebClient
+
+    Private Sub Form_Launcher_Shown(sender As Object, e As EventArgs) Handles MyBase.Shown
+        Dim thread As New Thread(
+            Sub()
+                ListBox_status.Items.Add("Starting Launcher...")
+                check_updates()
+            End Sub
+        )
+
+        thread.Start()
+    End Sub
+
+    Private Sub check_updates()
+        ListBox_status.Items.Add("Looking for Updates...")
+        Dim server_ver As String
+        Dim local_ver As String
+
+        server_ver = client.DownloadString(version_address)
+        ListBox_status.Items.Add("Newest Version is " + server_ver)
+
+        local_ver = My.Computer.FileSystem.ReadAllText(local_ver_address)
+        ListBox_status.Items.Add("Local Version is " + local_ver)
+
+        If String.Compare(server_ver, local_ver) <> 0 Then
+            ListBox_status.Items.Add("Update available!")
+            start_update()
+            My.Computer.FileSystem.WriteAllText(local_ver_address, server_ver, False)
+        Else
+            ListBox_status.Items.Add("No Update available!")
+        End If
+    End Sub
+
+    Private Sub start_update()
+        ListBox_status.Items.Add("Start Update...")
+        ListBox_status.Items.Add("Downloading Update...")
+        My.Computer.Network.DownloadFile(download_address, "game.zip")
+        ListBox_status.Items.Add("Removing old Version...")
+        My.Computer.FileSystem.DeleteDirectory("game_data", FileIO.DeleteDirectoryOption.DeleteAllContents)
+        ListBox_status.Items.Add("Unpacking Update...")
+        ZipFile.ExtractToDirectory("game.zip", "game_data")
+        ListBox_status.Items.Add("Removing Update-Package")
+        My.Computer.FileSystem.DeleteFile("game.zip")
+        ListBox_status.Items.Add("Update finished")
+    End Sub
+
+    Private Sub Button_close_Click(sender As Object, e As EventArgs)
+        Me.Close()
+    End Sub
+
+    Private Sub Button_play_Click(sender As Object, e As EventArgs) Handles Button_play.Click
+        ChDir("game_data")
+        Dim PSI As New ProcessStartInfo("Galaxy Towers.exe")
+        PSI.WindowStyle = ProcessWindowStyle.Maximized 'or ProcessWindowStyle.Normal
+        Process.Start(PSI)
+        Me.Close()
+    End Sub
+
+    Private Sub Button_forceUpdate_Click(sender As Object, e As EventArgs) Handles Button_forceUpdate.Click
+        start_update()
+    End Sub
+
+    Private Sub Button_close_Click_1(sender As Object, e As EventArgs) Handles Button_close.Click
+        Me.Close()
+    End Sub
+End Class
